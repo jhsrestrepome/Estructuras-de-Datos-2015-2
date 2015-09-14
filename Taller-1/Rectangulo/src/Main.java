16ef80da7e10b93e03c9f602804641f5d8d6ba76
jnbustosr <jn.bustosr@hotmail.com>
2015-09-13 23:48:26 -0500

    Update Main.java

--- Taller-1/Rectangulo/src/Main.java
+++ Taller-1/Rectangulo/src/Main.java
@@ -6,34 +6,42 @@
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		Rectangulo rectangulo = new Rectangulo();
+		
 		double area1 = rectangulo.areaRectangulo();
 		double perimetro1 = rectangulo.perimetroRectangulo();
+		
 		System.out.println("El area es :" + area1);	//imprime el area de los valores por defecto.				
 		System.out.println("El perimetro es :" + perimetro1); //imprime el perimetro del rectangulo
 		rectangulo.setAlto(2);		//asigna un nuevo valor al alto del rectangulo
 		rectangulo.setLargo(2);		//asigna un nuevo valor al largo del rectangulo
+		
 		double area2 = rectangulo.areaRectangulo();
 		double perimetro2 = rectangulo.perimetroRectangulo();
+		
 		System.out.println("El area es :" +area2);
 		System.out.println("El perimetro es :" + perimetro2);
 		rectangulo.setAlto(-1);
 		rectangulo.setLargo(2);
+		
 		double area3 = rectangulo.areaRectangulo();
 		double perimetro3 = rectangulo.perimetroRectangulo();
 		System.out.println("El area es :" +area3);
 		System.out.println("El perimetro es :" + perimetro3);
 		rectangulo.setAlto(21);
 		rectangulo.setLargo(2);
+		
 		double area4 = rectangulo.areaRectangulo();
 		double perimetro4 = rectangulo.perimetroRectangulo();
+		
 		System.out.println("El area es :" +area4);
 		System.out.println("El perimetro es :" + perimetro4);
 		rectangulo.setAlto(4);
 		rectangulo.setLargo(4);
+		
 		double area5 = rectangulo.areaRectangulo();
 		double perimetro5 = rectangulo.perimetroRectangulo();
+		
 		System.out.println("El area es :" +area5);
 		System.out.println("El perimetro es :" + perimetro5);
 		
